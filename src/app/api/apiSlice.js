9a7a23455fbffc1d867519b16525d2aec0c912f1
programmingherorubel <programmingherorubel@gmail.com>
2023-09-03 20:30:31 +0600

    beds filter working

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:9000/',
+    baseUrl: 'https://job-task-backend-programmingherorubel.vercel.app/',
   }),
   endpoints: (builder) => ({
     getCategory: builder.query({
