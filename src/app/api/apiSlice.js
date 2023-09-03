13bb0c7ab422d64676b64bb0abb35ca683874ac0
programmingherorubel <programmingherorubel@gmail.com>
2023-09-03 20:41:02 +0600

    change url my website

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
+    // main url 
     baseUrl: 'https://job-task-backend-programmingherorubel.vercel.app/',
   }),
   endpoints: (builder) => ({
